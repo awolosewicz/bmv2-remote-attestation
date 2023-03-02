0873c9c7e1cf659721018667e321623c1ca39fe8
awolosewicz <112141084+awolosewicz@users.noreply.github.com>
2023-03-02 04:44:32 -0600

    case sensitivity

--- targets/simple_switch/simple_switch.cpp
+++ targets/simple_switch/simple_switch.cpp
@@ -740,7 +740,7 @@ SimpleSwitch::egress_thread(size_t worker_id) {
       phv->get_field("remoteAttestation_h.ra_tables").set(ra_registers[1]);
       phv->get_field("remoteAttestation_h.ra_program").set(ra_registers[2]);
       phv->get_header("remoteAttestation").mark_valid();
-      BMLOG_DEBUG_PKT(*packet, "Is RemoteAtt Valid? {}", phv->get_header("remoteAttestation").is_Valid());
+      BMLOG_DEBUG_PKT(*packet, "Is RemoteAtt Valid? {}", phv->get_header("remoteAttestation").is_valid());
     }
     else {
       bm::header_id_t raHeaderID = phv->get_headers_size();
@@ -758,7 +758,7 @@ SimpleSwitch::egress_thread(size_t worker_id) {
       phv->get_field("remoteAttestation.ra_tables").set(ra_registers[1]);
       phv->get_field("remoteAttestation.ra_program").set(ra_registers[2]);
       phv->get_header("remoteAttestation").mark_valid();
-      BMLOG_DEBUG_PKT(*packet, "Is RemoteAtt Valid? {}", phv->get_header("remoteAttestation").is_Valid());
+      BMLOG_DEBUG_PKT(*packet, "Is RemoteAtt Valid? {}", phv->get_header("remoteAttestation").is_valid());
       /*
       bm::header_id_t raHeaderID = phv->get_headers_size();
       std::set<int> arithSet;
